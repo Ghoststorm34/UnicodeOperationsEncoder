888b06294a58f59f653f111693f1a92961c6b3b9
Alexander Ayers <aayers4@my,westga.edu>
2020-09-26 19:42:55 -0400

    Change #1:
    Added private helper methods for encoding UTF=8 with one, two, three,
    and four bytes.

--- src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -74,23 +74,67 @@ public class Codepoint {
 		return this.decodedHex >= 0x0800 && this.decodedHex <= 0xffff;
 	}
 
-	private boolean checkIfHexStringIsFourBytesUTF8() {
-		return this.decodedHex >= 0x10000 && this.decodedHex <= 0x10ffff;
-	}
+//	private boolean checkIfHexStringIsFourBytesUTF8() {
+//		return this.decodedHex >= 0x10000 && this.decodedHex <= 0x10ffff;
+//	}
 
 	private String encodeUTF8OneByte() {
-		return "";
+		int oneByteEncoding = this.decodedHex & 0b000000000000111111111;
+		String encodedString = String.format("%X", oneByteEncoding);
+		
+		return encodedString;
 	}
 
 	private String encodeUTF8TwoBytes() {
-		return "";
+		int upperFiveBits = this.decodedHex >> 6;
+		int lowerSixBits = this.decodedHex & 0b00000111111;
+		
+		int upperTwoByteEncoding = 0b11000000 | upperFiveBits;
+		int lowerTwoByteEncoding = 0b10000000 | lowerSixBits;
+		
+		int fullTwoByteEncoding = (upperTwoByteEncoding << 8) | lowerTwoByteEncoding;
+		String encodedString = String.format("%X", fullTwoByteEncoding);
+		
+		return encodedString;
 	}
 
 	private String encodeUTF8ThreeBytes() {
-		return "";
+		int upperFourBits = this.decodedHex >> 12;
+		int middleSixBits = this.decodedHex >> 6;
+		middleSixBits = this.decodedHex & 0b0000000000111111;
+		int lowerSixBits = this.decodedHex & 0b0000000000111111;
+		
+		int upperThreeByteEncoding = 0b11100000 | upperFourBits;
+		int middleThreeByteEncoding = 0b10000000 | middleSixBits;
+		int lowerThreeBitsEncoding = 0b10000000 | lowerSixBits;
+		
+		int fullThreeByteEncoding = (upperThreeByteEncoding << 8) | middleThreeByteEncoding;
+		fullThreeByteEncoding = (fullThreeByteEncoding << 8) | lowerThreeBitsEncoding;
+		
+		String encodedString = String.format("%X", fullThreeByteEncoding);
+		
+		return encodedString;
 	}
 
 	private String encodeUTF8FourBytes() {
-		return "";
+		int firstQuarterOfBits = this.decodedHex >> 18;
+		int secondQuarterOfBits = this.decodedHex >> 12;
+		secondQuarterOfBits = secondQuarterOfBits & 0b000000000000000111111;
+		int thirdQuarterOfBits = this.decodedHex >> 6;
+		thirdQuarterOfBits = thirdQuarterOfBits & 0b000000000000000111111;
+		int fourthQuarterOfBits = this.decodedHex & 0b000000000000000111111;
+		
+		int firstQuarterFourByteEncoding = 0b11110000 | firstQuarterOfBits;
+		int secondQuarterFourByteEncoding = 0b10000000 | secondQuarterOfBits;
+		int thirdQuarterFourByteEncoding = 0b10000000 | thirdQuarterOfBits;
+		int fourthQuarterFourByteEncoding = 0b10000000 | fourthQuarterOfBits;
+		
+		long fullFourByteEncoding = (firstQuarterFourByteEncoding << 8) | secondQuarterFourByteEncoding;
+		fullFourByteEncoding = (fullFourByteEncoding << 8) | thirdQuarterFourByteEncoding;
+		fullFourByteEncoding = (fullFourByteEncoding << 8) | fourthQuarterFourByteEncoding;
+		
+		String encodedString = String.format("%X", fullFourByteEncoding);
+		
+		return encodedString;
 	}
 }
