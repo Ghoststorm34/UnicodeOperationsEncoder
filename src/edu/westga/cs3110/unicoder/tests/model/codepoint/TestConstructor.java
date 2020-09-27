3632a44059abb038a6370d68abdb2d68da407e32
Alexander Ayers <aayers4@my.westga.edu>
2020-09-27 15:12:57 -0400

    Change #5:
    Added full unit testing for constructors. Additionally, made empty
    testing classes for toUTF8(), toUTF16(), and toUTF32().

--- src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestConstructor.java
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestConstructor.java
@@ -1,5 +1,28 @@
 package edu.westga.cs3110.unicoder.tests.model.codepoint;
 
-public class TestConstructor {
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+
+class TestConstructor {
+
+	@Test
+	void testNullHexString() {
+		assertThrows(IllegalArgumentException.class, () -> new Codepoint(null));
+	}
+
+	@Test
+	void testEmptyHexString() {
+		assertThrows(IllegalArgumentException.class, () -> new Codepoint(""));
+	}
+	
+	@Test
+	void testValidConstructor() {
+		Codepoint validData = new Codepoint("FFFF");
+		
+		assertEquals("FFFF", validData.getHexString());
+	}
 }
